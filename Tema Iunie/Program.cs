362158f2433609471d1bc717852cd416e9ddc67f
puscasvlad <38328839+puscasvlad@users.noreply.github.com>
2018-06-28 20:18:37 +0300

    Add project files.

---
+++ Tema Iunie/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema_Iunie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //introducem un user name si parola validare, incercam de 3 ori.
+
+            Console.WriteLine("Introduceti usernameul: ");
+            string user = Console.ReadLine();
+            Console.WriteLine("Introduceti parola: ");
+            string parola = Console.ReadLine();
+
+            Validator validator = new Validator();
+
+            if(validator.Valideaza(user,parola))
+            {
+                Console.WriteLine("Succes");
+            }
+            else
+            {
+                Console.WriteLine("Date gresite");
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tema Iunie/Validator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema_Iunie
+{
+    class Validator
+    {
+        private string Trueusername = "Vlad";
+        private string TruePassword = "Secret";
+        private int contor = 0;
+
+        public bool Valideaza(string username,string parola)
+        {
+            if(this.Trueusername == username && this.TruePassword==parola)
+            {
+                return true;
+            }
+            else
+            {
+                contor++;
+                return false;
+            }
+        }
+
+    }
+}
